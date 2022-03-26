7c22e6b060dc733888f987260b7b42ba600d01fb
itsmemohit <mohitdangwal016.md@gmail.com>
2022-03-26 17:47:20 +0530

    profile

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -16,4 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
